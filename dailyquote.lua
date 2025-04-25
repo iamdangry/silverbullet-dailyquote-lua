2669f17711670a9e1f99a95d0069f024f04d69b0
iamdangry <46516977+iamdangry@users.noreply.github.com>
2025-04-25 15:31:22 +1000

    Add comments to begining of script

--- dailyquote.lua
+++ dailyquote.lua
@@ -1,3 +1,8 @@
+-- Description: Daily Quote plugin for SilverBullet
+-- Author: iamdangry
+-- Version: 0.1.0
+-- License: MIT
+-- Repo: https://github.com/iamdangry/silverbullet-dailyquote-lua
 local version = "0.1.0"
 
 command.define {
