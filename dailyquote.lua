9810548c4d5a663dee86474f40fd462abb072c91
iamdangry <46516977+iamdangry@users.noreply.github.com>
2025-04-25 16:06:08 +1000

    Update workflow to create gist in SilverBullet format

---
+++ dailyquote.lua
@@ -0,0 +1,15 @@
+local version = "0.1.0"
+
+dailyQuote = {}
+dailyQuote.config = config.get("dailyquote", {})
+dailyQuote.includeTags = dailyQuote.includeTags or {}
+dailyQuote.excludeTags = dailyQuote.excludeTags or {}
+dailyQuote.includeAuthors = dailyQuote.includeAuthors or {}
+dailyQuote.excludeAuthors = dailyQuote.excludeAuthors or {}
+
+command.define {
+    name = "Daily Quote: Version",
+    run = function()
+        editor.flashNotification "Version: " .. version
+    end
+}
