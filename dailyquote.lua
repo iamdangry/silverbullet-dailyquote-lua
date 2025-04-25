f862e35829c95e4f219ce6090328edd28fe1e90b
iamdangry <46516977+iamdangry@users.noreply.github.com>
2025-04-25 16:40:02 +1000

    Added quote import

--- dailyquote.lua
+++ dailyquote.lua
@@ -13,3 +13,35 @@ command.define {
         editor.flashNotification("Version: " .. version)
     end
 }
+
+command.define {
+    name = "Daily Quote: Import",
+    run = function()
+        importQuotes()
+    end
+}
+
+function importQuotes()
+    if not space.fileExists("quotes.json") then
+        editor.flashNotification("Importing quotes...")
+        local quotes = http.request("https://raw.githubusercontent.com/iamdangry/silverbullet-dailyquote/refs/heads/main/quotes.json")
+        if quotes then
+            json = space.writeFile("quotes.json", quotes)
+            if json then
+                dailyQuote.quotes = json
+                editor.flashNotification("Quotes imported successfully!")
+            else
+                editor.flashNotification("Failed to parse quotes.")
+            end
+        else
+            editor.flashNotification("Failed to fetch quotes.")
+        end
+    end
+end
+
+event.listen {
+    name = "system:ready",
+    run = function()
+        importQuotes()
+    end
+}
